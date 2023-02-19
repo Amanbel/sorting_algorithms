86e514b0f5d955b1d96bed6da1718761fd118375
Amanbel <amanuel11belachew@gmail.com>
2023-02-20 01:34:36 +0300

    sort_alg

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,7 +13,8 @@ void bubble_sort(int *array, size_t size)
 {
 	unsigned int i;
 	unsigned int j;
-	int val = NULL;
+	int val;
+	int m = 0;
 	unsigned int k;
 
 	for (i = 0; i < size; i++)
@@ -25,6 +26,7 @@ void bubble_sort(int *array, size_t size)
 				val = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = val;
+				m = 1;
 			}
 			for (k = 0; k < size; k++)
 			{
@@ -36,7 +38,7 @@ void bubble_sort(int *array, size_t size)
 			}
 			putchar('\n');
 		}
-		if (val != NULL)
+		if (m == 0)
 		{
 			break;
 		}
