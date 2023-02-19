92629ffefc193701ce586d505378ef0757a434b0
Amanbel <amanuel11belachew@gmail.com>
2023-02-20 01:41:02 +0300

    sort_alg

--- sort.h
+++ sort.h
@@ -7,16 +7,15 @@
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
-
-void print_list(const listint_t *list);
-void print_array(const int *array, size_t size);
-void bubble_sort(int *array, size_t size);
-
 typedef struct listint_s
 {
 	const int n;
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
+
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
+void bubble_sort(int *array, size_t size);
 
 #endif
