f5dfd275b7d53e619d07fa0d9a7b3289f2b8d3f0
Amanbel <amanuel11belachew@gmail.com>
2023-02-21 21:45:24 +0300

    sort_alg

---
+++ 2-selection_sort.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * selection_sort - a function that uses selection sort 
+ * algorithm to sort an array
+ * @array: array given by the user
+ * @size: size of the given array
+ * Return: Nothing
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	unsigned int i;
+	unsigned int j;
+	int small;
+	int m = -1;
+	int val;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		small = array[i];
+		for (j = i; j < size; j++)
+		{
+			if (array[j] < small)
+			{
+				small = array[j];
+				m = j;
+			}
+		}
+		if (m >= 0)
+		{
+			val = array[i];
+			array[i] = small;
+			array[m] = val;
+			print_array(array, size);
+		}
+		else
+			break;
+	}
+}
