85ccaf4e25f17d54a805983e4f948f980659a884
Amanbel <amanuel11belachew@gmail.com>
2023-02-24 20:55:50 +0300

    sort_alg

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -33,12 +33,12 @@ void shell_sort(int *array, size_t size)
 	unsigned int j;
 	unsigned int n;
 	unsigned int m;
-	
+
 	if (array == NULL || size < 2)
 		return;
 	n = 1;
 
-	while (n < size)
+	while (n < size / 3)
 	{
 		n = n * 3 + 1;
 	}
@@ -56,8 +56,7 @@ void shell_sort(int *array, size_t size)
 				m--;
 			}
 		}
-		if (n < size)
-			print_array(array, size);
-		n = (n - 1) / 3;
+		print_array(array, size);
+		n /= 3;
 	}
 }
