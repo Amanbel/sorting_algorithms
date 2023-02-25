13c33f345dac872200a4f4518a1a936c28ec25ad
Amanbel <amanuel11belachew@gmail.com>
2023-02-25 19:17:15 +0300

    sort_alg

--- sort.h
+++ sort.h
@@ -1,6 +1,8 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stddef.h>
+
 /**
  * struct listint_s - Doubly linked list node
  * @n: Integer stored in the node
@@ -23,5 +25,6 @@ void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
 
 #endif
